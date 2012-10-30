c9035befed6c6e04a95e5b303e05f4555e67eb4b
LoreLLo <lorello@openweb.it>
2012-10-30 10:57:38 +0100

    minor fix and reduced redundancy

--- puppetmaster-bootstrap.sh
+++ puppetmaster-bootstrap.sh
@@ -1,26 +1,14 @@
 #!/bin/bash
 
-DISTRO=$(lsb_release -c -s)
-PUPPETHOSTS="puppethardy.vagrant.local puppetlucid.vagrant.local puppetprecise.vagrant.local"
+. /vagrant/puppetclient-bootstrap.sh
 
-if [ ! -f puppetlabs-release-$DISTRO.deb ]; then
-    echo "Downloading PuppetLabs release package"
-    wget -q http://apt.puppetlabs.com/puppetlabs-release-$DISTRO.deb
-fi
-
-if $(dpkg -l | grep -q puppetlabs-release); then
-    echo "PuppetLabs repositories package already installed."
-else
-    echo "Adding PuppetLabs repositories..."
-    dpkg -i puppetlabs-release-$DISTRO.deb
-    update-rc.d puppetmaster enable
-fi
 
 if $(dpkg -l | grep -q puppetmaster); then
     echo "puppetmaster already installed."
 else
     echo "Installing puppetmaster..."
     aptitude -q -y install puppetmaster 
+    update-rc.d puppetmaster enable
 fi
 
 if [ ! $(egrep '^autosign=' /etc/puppet/puppet.conf) ]; then
@@ -32,14 +20,6 @@ if [ ! $(egrep '^certname=' /etc/puppet/puppet.conf) ]; then
     echo "Setting certname..."
     echo "certname=puppetmaster.vagrant.local" >> /etc/puppet/puppet.conf
 fi
-
-for PUPPET_HOST in $PUPPETHOSTS; do
-    HOST_CERT=/var/lib/puppet/ssl/ca/signed/$PUPPET_HOST.pem
-    if [ -f $HOST_CERT ]; then
-        echo "Cleaning certificates for $PUPPET_HOST"
-        puppetca --clean $PUPPET_HOST
-    fi
-done
 
 [ -x /vagrant/puppetmaster-code.sh ] && /vagrant/puppetmaster-code.sh
 
