70a54844253ee5a3a8546768ececda4d80ff5a3d
LoreLLo <lorello@openweb.it>
2012-11-29 09:49:04 +0100

    removed comments

--- package.sh
+++ package.sh
@@ -1,5 +1,10 @@
+
+WHAT=$1
+
+
+
 vagrant package puppetmaster --output puppetmaster-softec.box --vagrantfile Vagrantfile.package
 echo mv -i puppetmaster-softec.box ~/Dropbox/VagrantBoxes/puppetdev/puppetmaster-softec.box
-#vagrant package puppetprecise --output ~/Dropbox/VagrantBoxes/puppetdev/puppetprecise.box
-#vagrant package puppetlucid --output ~/Dropbox/VagrantBoxes/puppetdev/puppetlucid.box
-#vagrant package puppethardy --output ~/Dropbox/VagrantBoxes/puppetdev/puppethardy.box
+vagrant package puppetprecise --output ~/Dropbox/Public/vagrant-boxes/puppetprecise.box
+vagrant package puppetlucid --output ~/Dropbox/Public/vagrant-boxes/puppetlucid.box
+vagrant package puppethardy --output ~/Dropbox/Public/vagrant-boxes/puppethardy.box
